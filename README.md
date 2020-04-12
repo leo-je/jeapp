01041a845d5fc438d6420d76c852994743b9988c
shengte <shengte@shengtedeMacBook-Pro.local>
2020-04-12 23:19:33 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # jeapp
 react native app test
+
+# 安装device-info
+npm install --save core-js@3
+npm install --save react-native-device-info
+
+# 启动
+npm start
---
+++ gitinit.sh
@@ -0,0 +1,7 @@
+git add .
+git commit -m "init"
+git remote add origin https://github.com/leo-je/jeapp.git
+git pull origin master --allow-unrelated-histories
+git add .
+git commit -m "init"
+git push --set-upstream origin master
